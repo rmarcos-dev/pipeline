af0d9f35262053e54b693c1ed999080fd239e237
Rafa Marcos <rmarcos@appsur.com>
2026-03-25 10:49:27 +0000

    test: comprobando despliegue automatico

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Al usar /pipeline/ en Nginx, Flask debe responder a la raíz interna
 @app.route('/')
 def home():
-    return "<h1>¡App Desplegada con Éxito!</h1><p>Hola Rafael, esta web corre dentro de Docker.</p>"
+    return """
+    <h1>¡Pipeline Automatizado!</h1>
+    <p>Hola Rafael, este cambio se ha hecho mediante Git Push.</p>
+    <hr>
+    <small>Desplegado automáticamente por GitHub Actions y servido por Nginx.</small>
+    """
 
 if __name__ == "__main__":
-    # 0.0.0.0 es vital para que sea accesible desde fuera del contenedor
     app.run(host='0.0.0.0', port=8000)
